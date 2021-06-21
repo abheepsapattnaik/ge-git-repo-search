ea9e196f5051a48f947348065db24147c6f35b2b
Abheepsa Pattnaik <abheepsapattnaik@gmail.com>
2021-06-21 18:20:55 +0530

    #000 | Abheepsa | Adds dependencies for material ui and restructure

---
+++ src/common/component/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core';
+import {HashRouter, Route, Switch} from "react-router-dom";
+import Home from "../../features/home/component/home";
+
+const theme = createMuiTheme({
+        palette: {
+            secondary: {
+                main: '#C62828'
+            }
+        },
+    })
+;
+
+const App = () => {
+    return <MuiThemeProvider theme={theme}>
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+            </Switch>
+        </HashRouter>
+
+    </MuiThemeProvider>;
+
+}
+
+
+export default App;
